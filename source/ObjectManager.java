9d92f3197cd72198255a9f59720a73b76717627f
League Student <league@iMac-109.attlocal.net>
2020-02-08 12:57:55 -0800

    Game
    :smile:

---
+++ source/ObjectManager.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class ObjectManager implements ActionListener{
+
+	Crosshair crosshair;
+	Random random = new Random();
+	int score = 0;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
